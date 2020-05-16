510414bf318e8d900b9b05ecd0289723419e5c61
nopesir <s254300@studenti.polito.it>
2020-05-16 16:29:46 +0200

    fixed security issues

---
+++ oauth2-authorization-server/src/main/java/com/homework/oauth2/authorization/server/OAuth2ServerApplication.java
@@ -0,0 +1,8 @@
+package com.homework.oauth2.authorization.server;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class OAuth2ServerApplication extends Application {
+}
